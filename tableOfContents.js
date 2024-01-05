c5f0dda5012f8e6cd81091191ee9fdc9c7a4fc0d
TuurVanBergen <75333376+TuurVanBergen@users.noreply.github.com>
2024-01-05 22:31:37 +0100

    added the right link to the api

--- tableOfContents.js
+++ tableOfContents.js
@@ -2,7 +2,7 @@ import Gallery from "./gallery.js";
 let allGalleries = [];
 
 //get the data from "http://localhost:3000/Table-of-Contents" and makes Gallery objects with it.
-getData("http://localhost:3000/Table-of-Contents", "GET", ).then((data) => {
+getData("https://brussel-in-beeld.onrender.com/Table-of-Contents", "GET", ).then((data) => {
     for (let i = 0; i < data.length; i++) {
         let Galleries = new Gallery(data[i]._id, data[i].title, data[i].date, data[i].page,data[i].category, data[i].opening_hours, data[i].price, data[i].location, data[i].accesability, data[i].facebook_link,data[i].instagram_link,data[i].website_link,data[i].text_1_title,data[i].text_1,data[i].text_2_title,data[i].text_2);
         allGalleries.push(Galleries);
