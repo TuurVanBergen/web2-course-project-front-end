950f2e226b5de82bd6520874c5ee8638d36f2840
TuurVanBergen <75333376+TuurVanBergen@users.noreply.github.com>
2024-01-05 23:24:07 +0100

    added new api link

--- artworks.js
+++ artworks.js
@@ -2,7 +2,7 @@ import Artwork from "./artwork.js";
 let allArtworks = [];
  
 //get the data from "http://localhost:3000/Artwork" and makes Artwork objects with it.
-getData("http://localhost:3000/Artwork", "GET", ).then((data) => {
+getData("https://brussel-in-beeld.onrender.com/Artwork", "GET", ).then((data) => {
     for (let i = 0; i < data.length; i++) {
         let Artworks = new Artwork(data[i]._id, data[i].title, data[i].text, data[i].img1,data[i].img2, data[i].img3, data[i].img4, data[i].img5, data[i].img6, data[i].img7);
         allArtworks.push(Artworks);
